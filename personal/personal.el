53e6591f1dcd8ec1ad3fa0a2eaf39893995b9f93
Jose Plana <jplana@gmail.com>
2013-09-04 00:24:02 +0200

    Added missing spanish keyboard mapping

---
+++ personal/personal.el
@@ -0,0 +1,7 @@
+;; Key modifiers for Mac OS X Emacs.app in spanish MBP keyboard
+(global-set-key (kbd "s-1") "|")
+(global-set-key (kbd "s-2") "@")
+(global-set-key (kbd "s-3") "#")
+(global-set-key (kbd "s-º") "\\")
+(global-set-key (kbd "s-ç") "}")
+(global-set-key (kbd "s-+") "]")
